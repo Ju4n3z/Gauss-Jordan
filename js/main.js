add7e1c51cfdeb09503e08ea41b44abc067faad3
Ju4n3z <juanesteban.gomezgarcia@gmail.com>
2023-06-19 22:39:38 -0500

    feat: :sparkles: Termino del proyecto
    Se completó el proyecto de inversa con Gauss-Jordan

--- js/main.js
+++ js/main.js
@@ -42,9 +42,45 @@ const crearMatriz = (tamaño) =>{
                 };
         }
         console.log(matrizN);
-        //calcularMatriz(matriz);
+        calcularMatriz(matriz);
     })
 }
+
+//Hacer Gauss-Jordan
+
+const calcularMatriz = (matriz) => {
+    const filas = matriz.length;
+    const columnas = matriz[0].length;
+    
+    for (let i = 0; i < filas; i++) {
+      // Pivoteo parcial
+      let maxRow = i;
+      for (let j = i + 1; j < filas; j++) {
+        if (Math.abs(matriz[j][i]) > Math.abs(matriz[maxRow][i])) {
+          maxRow = j;
+        }
+      }
+      [matriz[i], matriz[maxRow]] = [matriz[maxRow], matriz[i]];
+      
+      // Hacer el pivote en la fila i
+      let pivot = matriz[i][i];
+      for (let j = i; j < columnas; j++) {
+        matriz[i][j] /= pivot;
+      }
+      
+      // Eliminar otras entradas en la columna del pivote
+      for (let k = 0; k < filas; k++) {
+        if (k !== i) {
+          let factor = matriz[k][i];
+          for (let j = i; j < columnas; j++) {
+            matriz[k][j] -= factor * matriz[i][j];
+          }
+        }
+      }
+    }
+    
+    console.log(matriz);
+  }
 
 let formulario = document.querySelector('#formulario');
 
